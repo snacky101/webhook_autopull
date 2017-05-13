2dd02334b8057df72b84ecb0c59093a98a534118
snacky <cliche90@naver.com>
2017-05-13 23:28:30 +0900

    test

--- index.js
+++ index.js
@@ -11,35 +11,33 @@ let port = 8081;
 
 app.post('/push', (req, res) => {
     console.log('request received');
+    res.status(400).set('Content-Type', 'application/json');
 
     let jsonString = '';
     req.on('data', function (data) {
         jsonString += data;
     });
 
-    req.on('end', function () {
-        res.set('Content-Type', 'application/json');
-        console.log('jsonString : ' + jsonString);
-        let hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
+    console.log(req.body);
+    let hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
 
-        if (hash != req.get('x-hub-signature')) {
-            console.log('invalid key');
-            console.log(hash);
-            let data = JSON.stringify({ "error": "invalid key", key: hash });
-            return res.status(400).end(data);
-        }
+    if (hash != req.get('x-hub-signature')) {
+        console.log('invalid key');
+        console.log(hash);
+        let data = JSON.stringify({ "error": "invalid key", key: hash });
+        res.status(400).send(data);
+    }
 
-        console.log("running hook.sh");
+    console.log("running hook.sh");
 
-        let deploySh = spawn('sh', ['hook.sh']);
-        deploySh.stdout.on('data', function (data) {
-            let buff = new Buffer(data);
-            console.log(buff.toString('utf-8'));
-        });
-
-        let data = JSON.stringify({ "success": true });
-        return res.status(200).end(data);
+    let deploySh = spawn('sh', ['hook.sh']);
+    deploySh.stdout.on('data', function (data) {
+        let buff = new Buffer(data);
+        console.log(buff.toString('utf-8'));
     });
+
+    let data = JSON.stringify({ "success": true });
+    return res.status(200).end(data);
 
 
 });
