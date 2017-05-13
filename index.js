0ceab925829e263f46b2f53bf75a38f230f1cc78
snacky <cliche90@naver.com>
2017-05-13 23:03:52 +0900

    test

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var spawn   = require('child_process').spawn;
-var crypto = require('crypto');
-var app = express();
+let express = require('express');
+let bodyParser = require('body-parser');
+let spawn = require('child_process').spawn;
+let crypto = require('crypto');
+let app = express();
 
 app.use(bodyParser.json());
 
-var secret = "amazingkey";
-var port = 8081;
+let secret = "amazingkey";
+let port = 8081;
 
 app.post('/push', (req, res) => {
     console.log('request received');
     res.set('Content-Type', 'application/json');
 
-    var jsonString = req.body;
-    console.log('jsonString : ' + jsonString);
-    var hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
-
-    if(hash == req.get('x-hub-signature')){
-        console.log('!!!!!!!!!!!!!!!');
-    }
-    if(hash != req.get('x-hub-signature')){
-        console.log('invalid key');
-	console.log(hash);
-        var data = JSON.stringify({"error": "invalid key", key: hash});
-        res.send(data);
-    }
-
-    console.log("running hook.sh");
-
-    var deploySh = spawn('sh', ['hook.sh']);
-    deploySh.stdout.on('data', function(data){
-        var buff = new Buffer(data);
-        console.log(buff.toString('utf-8'));
+    let jsonString = '';
+    req.on('data', function (data) {
+        jsonString += data;
     });
 
-    var data = JSON.stringify({"success": true});
-    res.send(data);
+    req.on('end', function () {
+        console.log('jsonString : ' + jsonString);
+        let hash = "sha1=" + crypto.createHmac('sha1', secret).update(jsonString).digest('hex');
+
+        if (hash != req.get('x-hub-signature')) {
+            console.log('invalid key');
+            console.log(hash);
+            let data = JSON.stringify({ "error": "invalid key", key: hash });
+            return res.status(400).end(data);
+        }
+
+        console.log("running hook.sh");
+
+        let deploySh = spawn('sh', ['hook.sh']);
+        deploySh.stdout.on('data', function (data) {
+            let buff = new Buffer(data);
+            console.log(buff.toString('utf-8'));
+        });
+
+        let data = JSON.stringify({ "success": true });
+        return res.status(200).end(data);
+    });
+
+
 });
 
 app.listen(port, () => console.log('listen to ' + port + ' port'));
-//test2
